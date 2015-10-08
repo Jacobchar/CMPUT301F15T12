e61644b58c9a088016c1dffde0a3a3814544e190
Jacobchar <jj.charlebois@gmail.com>
2015-10-08 15:30:59 -0600

    Test cases on test cases on test cases

---
+++ UnitTests/testFriendsList.java
@@ -0,0 +1,39 @@
+public class testFriendsList extends ActivityInstrumentationTestCase2 {
+
+	public void testAddFriend() {
+		FriendList friendList = new FriendList();
+		Friend friend = new Friend("David", 12);
+		friendList.addBook(friend);
+		assertTrue(friendList.hasBook(friend));
+
+		Friend friend2 = new Friend("David", 12);
+		friendList.addBook(friend2);
+		int numFriends = friendList.numFriends();
+		assertEquals(1, numFriends);
+	}
+
+	public void testExterminateFriend() {
+		FriendList friendList = new FriendList();
+		Friend friend = new Friend("Bob", 14);
+		friendList.addBook(friend);
+		friendList.DeleteBook(friend);
+		assertFalse(friendList.hasBook(friend));
+	}
+
+	public void testHasFriend() {
+		FriendList friendList = new FriendList();
+		Friend friend = new Friend("Freddy", 16);
+		assertFalse(friendList.hasBook(friend));
+		friendList.addBook(friend);
+		assertTrue(friendList.hasBook(friend));
+	}
+	public void testNumFriends() {
+		FriendList friendList = new FriendList();
+		Friend friend1 = new Friend("David", 12);
+		friendList.addBook(friend1);
+		Friend friend2 = new Friend("Leroy", 14);
+		friendList.addBook(friend2);
+		int numFriends = friendList.numFriends();
+		assertEquals(2, numFriends);
+	}
+}
