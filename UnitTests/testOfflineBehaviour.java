320b81eca88d5515bb07d766ce022d92546b0c09
David <dav1d@shaw.ca>
2015-10-08 12:59:43 -0600

    Added offline behaviour tests file

---
+++ UnitTests/testOfflineBehaviour.java
@@ -0,0 +1,10 @@
+import android.test.ActivityInstrumentationTestCase2;
+
+public class testOfflineBehaviour extends ActivityInstrumentationTestCase2{
+	testPushOfflineChanges(){
+		// not sure how this could be done
+	}
+
+	testBrowseFriendsOffline(){
+		//  not sure how this would be done
+	}
