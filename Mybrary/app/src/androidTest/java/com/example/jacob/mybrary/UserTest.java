e80435cdc1af4ccbeb4c1866a671654791256182
Ben Schreiber <bschreib@ualberta.ca>
2015-10-29 17:36:32 -0600

    Created User and UserTest Class

---
+++ Mybrary/app/src/androidTest/java/com/example/jacob/mybrary/UserTest.java
@@ -0,0 +1,40 @@
+package com.example.jacob.mybrary;
+
+
+import android.test.ActivityInstrumentationTestCase2;
+
+//Tests for the User Class
+public class UserTest extends ActivityInstrumentationTestCase2{
+
+    public UserTest() {
+        super(User.class);
+    }
+
+    public void testCreateUser(){
+
+    }
+
+    public void testGetInventory(){
+
+    }
+
+    public void testGetUserID(){
+
+    }
+
+    public void testGetName(){
+
+    }
+
+    public void testGetPhoneNumber(){
+
+    }
+
+    public void testGetGender(){
+
+    }
+
+    public void testGetBio(){
+
+    }
+}
---
+++ Mybrary/app/src/main/java/com/example/jacob/mybrary/User.java
@@ -0,0 +1,5 @@
+package com.example.jacob.mybrary;
+
+//User Class
+public class User {
+}
