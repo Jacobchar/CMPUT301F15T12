ded4b88a81203d8c89cfc7b92ca94232fa149aa2
dktrotti <dktrotti@ualberta.ca>
2015-10-31 12:16:57 -0600

    Fixed quotation marks

--- UnitTests/testConfiguration.java
+++ UnitTests/testConfiguration.java
@@ -13,16 +13,16 @@ public class testConfiguration extends ActivityInstrumentationTestCase2{
 	public testEditProfileName(){
 	// Use Case 7.2
 		Configuration config  = new Configuration();
-		User user = new User(“Larry”, 1234);
-		assertTrue(user.getName().equals(“Larry”);
-		config.editName(“Bob”);
-		assertTrue(user.getName().equals(“Bob”);
+		User user = new User("Larry", 1234);
+		assertTrue(user.getName().equals("Larry");
+		config.editName("Bob");
+		assertTrue(user.getName().equals("Bob");
 	}
 
 	public testEditProfilePassword(){
 	// Use Case 7.2
 		Configuration config  = new Configuration();
-		User user = new User(“Larry”, 1234);
+		User user = new User("Larry", 1234);
 		assertTrue(user.getPassword() == 1234);
 		config.editPassword(4567);
 		assertTrue(user.getPassword() == 4567);
--- UnitTests/testPhotographsOfItems.java
+++ UnitTests/testPhotographsOfItems.java
@@ -4,7 +4,7 @@ public class testPhotographsOfItems extends ActivityInstrumentationTestCase2{
 	//Use Case 5.1
 	public void testAddPhoto(){
 		Book book = new Book();
-		String photoFile = “filename”;
+		String photoFile = "filename";
 		// assuming items can default to having no photo
 		assertFalse(book.hasPhoto(photoFile));
 		book.addPhoto(filename);
@@ -13,14 +13,14 @@ public class testPhotographsOfItems extends ActivityInstrumentationTestCase2{
 
 	public void testViewPhoto(){
 		Book book = new Book();
-		String photoFile = “filename”;
+		String photoFile = "filename";
 		book.add(photoFile)
 		assertTrue(book.hasPhoto());
 	}
 	// Use Case 5.2
 	public void testDeletePhoto(){
 		Book book = new Book();
-		String photoFile = “filename”;
+		String photoFile = "filename";
 		book.addPhoto(filename);
 		assertTrue(book.hasPhoto(photoFile);
 		item.deletePhoto();
@@ -29,11 +29,11 @@ public class testPhotographsOfItems extends ActivityInstrumentationTestCase2{
 	// Use Case 5.3
 	public void testCheckPhotoSize(){
 		Book book = new Book();
-		String photoFile = “filename”;
+		String photoFile = "filename";
 		book.addFile(photoFile);
 		assertTrue(book.checkFileSize() < 65536);
 	}
 	// Use Case 5.4
 	public void testManualDownload(){
-		// Not sure how to test yet
+		// Not sure how to test
 	}
