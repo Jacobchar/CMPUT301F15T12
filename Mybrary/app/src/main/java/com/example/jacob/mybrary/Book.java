11365b221fc7a9aa9223e2761f5b115aebb2914e
oksmarty <hessdorf@ualberta.ca>
2015-11-01 18:16:00 -0700

    Created book class.

---
+++ Mybrary/app/src/main/java/com/example/jacob/mybrary/Book.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class Book {
+
+	private String name;
+    private Integer quantity;
+    private String category;
+    private boolean sharedWithOthers;
+    private Collection<String> comments;
+    private UUID itemID;
+    private Photo photo;
+    private ArrayList observers;
+
+    public void Book(String name, Integer quantity, String category, boolean sharedWithOthers){
+        this.name = name;
+        this.quantity = quantity;
+        this.category = category;
+        this.sharedWithOthers = sharedWithOthers;
+
+        this.itemID = generateNewUUID();
+    }
+
+    UUID generateNewUUID(){
+        UUID id = new UUID();
+        return id;
+    }
+
+    void addNewComment(String comment){
+        this.comments.add(comment);
+    }
+
+    void updateObservers(){
+
+    }
+
+}
