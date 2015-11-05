a23d3e415e832843affe3a61ec2629e3ba1b7c9e
David <dav1d@shaw.ca>
2015-11-04 17:17:05 -0700

    Added checks ensuring  both users accepted the trade

--- Mybrary/app/src/androidTest/java/com/example/jacob/mybrary/TradeTest.java
+++ Mybrary/app/src/androidTest/java/com/example/jacob/mybrary/TradeTest.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 /**
  * Created by davidross on 2015-11-02.
  * Test moving items from user1 to user2
- * Assumes the trade has been previously accepted.
  */
 public class TradeTest extends ActivityInstrumentationTestCase2 {
     public TradeTest (){
@@ -31,6 +30,8 @@ public class TradeTest extends ActivityInstrumentationTestCase2 {
         assertTrue("Book 1 not added to user 1 before trade", user1Inv.hasBook(book1));
         assertTrue("Book 2 not added to user 2 before trade", user2Inv.hasBook(book2));
 
+
+
         ArrayList<Book> user1TradeOffer= new ArrayList();
         user1TradeOffer.add(book1);
 
@@ -38,6 +39,10 @@ public class TradeTest extends ActivityInstrumentationTestCase2 {
         user2TradeOffer.add(book2);
 
         Trade trade = new Trade(user1,user2);
+
+        trade.setUser1Accepted(true);
+        trade.setUser2Accepted(true);
+
         trade.tradeBooks(user1TradeOffer,user2TradeOffer);
 
         assertTrue("User 1 book 2 was not added after trade",user1Inv.hasBook(book2));
@@ -73,6 +78,8 @@ public class TradeTest extends ActivityInstrumentationTestCase2 {
         user2TradeOffer.add(book3);
 
         Trade trade = new Trade(user1,user2);
+        trade.setUser1Accepted(true);
+        trade.setUser2Accepted(true);
         trade.tradeBooks(user1TradeOffer,user2TradeOffer);
 
         assertTrue("User 1 book 2 was not added after trade", user1Inv.hasBook(book2));
@@ -105,12 +112,84 @@ public class TradeTest extends ActivityInstrumentationTestCase2 {
 
 
         Trade trade = new Trade(user1,user2);
+
+        trade.setUser1Accepted(true);
+        trade.setUser2Accepted(true);
+
         trade.tradeBooks(user1TradeOffer,user2TradeOffer);
 
 
-        assertFalse("User 1 book 1 was not removed after trade",user1Inv.hasBook(book1));
+        assertFalse("User 1 book 1 was not removed after trade", user1Inv.hasBook(book1));
 
         assertTrue("User 2 book 1 was not added after trade",user2Inv.hasBook(book1));
+    }
+
+    public void testUserNotAccepted(){
+        User user1 = new User("Bob", "12345", "Spirit","Likes Trades");
+        User user2 = new User("Harry","123456","Male","Dislikes Trades");
+
+        Book book1 = new Book("Files",1,"Documentary",false);
+        Book book2 = new Book("Murphy",1,"Law",false);
+
+        Inventory user1Inv = user1.getInventory();
+        user1Inv.addBook(book1);
+
+        Inventory user2Inv = user2.getInventory();
+        user2Inv.addBook(book2);
+
+        assertTrue("Book 1 not added to user 1 before trade", user1Inv.hasBook(book1));
+        assertTrue("Book 2 not added to user 2 before trade", user2Inv.hasBook(book2));
+
+        ArrayList<Book> user1TradeOffer= new ArrayList();
+        user1TradeOffer.add(book1);
+
+        ArrayList<Book> user2TradeOffer= new ArrayList();
+        user2TradeOffer.add(book2);
+
+        Trade trade = new Trade(user1,user2);
+
+        trade.setUser2Accepted(true);
+
+        trade.tradeBooks(user1TradeOffer, user2TradeOffer);
+
+        assertTrue("User 1 lost book 1", user1Inv.hasBook(book1));
+        assertFalse("User 1 gained book 2", user1Inv.hasBook(book2));
+
+        assertTrue("User 2 lost book 2",user2Inv.hasBook(book2));
+        assertFalse("User 2 gained book 1", user2Inv.hasBook(book1));
+    }
+
+    public void testNeitherUserAccepted(){
+        User user1 = new User("Bob", "12345", "Spirit","Likes Trades");
+        User user2 = new User("Harry","123456","Male","Dislikes Trades");
+
+        Book book1 = new Book("Files",1,"Documentary",false);
+        Book book2 = new Book("Murphy",1,"Law",false);
+
+        Inventory user1Inv = user1.getInventory();
+        user1Inv.addBook(book1);
+
+        Inventory user2Inv = user2.getInventory();
+        user2Inv.addBook(book2);
+
+        assertTrue("Book 1 not added to user 1 before trade", user1Inv.hasBook(book1));
+        assertTrue("Book 2 not added to user 2 before trade", user2Inv.hasBook(book2));
+
+        ArrayList<Book> user1TradeOffer= new ArrayList();
+        user1TradeOffer.add(book1);
+
+        ArrayList<Book> user2TradeOffer= new ArrayList();
+        user2TradeOffer.add(book2);
+
+        Trade trade = new Trade(user1,user2);
+
+        trade.tradeBooks(user1TradeOffer,user2TradeOffer);
+
+        assertTrue("User 1 lost book 1",user1Inv.hasBook(book1));
+        assertFalse("User 1 gained book 2",user1Inv.hasBook(book2));
+
+        assertTrue("User 2 lost book 2",user2Inv.hasBook(book2));
+        assertFalse("User 2 gained book 1",user2Inv.hasBook(book1));
     }
 
 }
--- Mybrary/app/src/main/java/com/example/jacob/mybrary/Trade.java
+++ Mybrary/app/src/main/java/com/example/jacob/mybrary/Trade.java
@@ -5,12 +5,13 @@ import java.util.List;
 /**
  * Created by davidross on 2015-11-02.
  * Completes trades by removing an item from one user1, and adding the same item to user2
- * This assumes that the trade has previously been accepted and will simply move the items.
  * Need to uncomment lines once removing books is implemented.
  */
 public class Trade {
     Inventory user1Inventory;
     Inventory user2Inventory;
+    Boolean user1Accepted = false;
+    Boolean user2Accepted = false;
 
     public Trade(User user1, User user2){
         user1Inventory = user1.getInventory();
@@ -18,11 +19,13 @@ public class Trade {
     }
 
     public void tradeBooks(List<Book> user1TradeOffer, List<Book> user2TradeOffer) {
-        addRecievedBooks(user2TradeOffer, user1Inventory);
-        addRecievedBooks(user1TradeOffer, user2Inventory);
+        if(user2Accepted  && user1Accepted) {
+            addRecievedBooks(user2TradeOffer, user1Inventory);
+            addRecievedBooks(user1TradeOffer, user2Inventory);
 
-        removeTradedBooks(user1TradeOffer, user1Inventory);
-        removeTradedBooks(user2TradeOffer, user2Inventory);
+            removeTradedBooks(user1TradeOffer, user1Inventory);
+            removeTradedBooks(user2TradeOffer, user2Inventory);
+        }
     }
 
     public void addRecievedBooks(List<Book> bookList, Inventory inventory){
@@ -38,5 +41,19 @@ public class Trade {
         }
     }
 
+    public void setUser1Accepted(Boolean accepted){
+        user1Accepted = accepted;
+    }
 
+    public void setUser2Accepted(Boolean accepted){
+        user2Accepted = accepted;
+    }
+
+    public Boolean getUser1Accepted(){
+        return this.user1Accepted;
+    }
+
+    public Boolean getUser2Accepted(){
+        return this.getUser2Accepted();
+    }
 }
