c133db25b03de666f1d47c4eb184ff182aa47796
David <dav1d@shaw.ca>
2015-11-04 17:56:11 -0700

    Added UUID to trades

--- Mybrary/app/src/main/java/com/example/jacob/mybrary/Trade.java
+++ Mybrary/app/src/main/java/com/example/jacob/mybrary/Trade.java
@@ -1,34 +1,41 @@
 package com.example.jacob.mybrary;
 
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by davidross on 2015-11-02.
  * Completes trades by removing an item from one user1, and adding the same item to user2
- * Need to uncomment lines once removing books is implemented.
  */
+
 public class Trade {
+    UUID user1;
+    UUID user2;
     Inventory user1Inventory;
     Inventory user2Inventory;
     Boolean user1Accepted = false;
     Boolean user2Accepted = false;
+    UUID tradeID;
 
     public Trade(User user1, User user2){
         user1Inventory = user1.getInventory();
         user2Inventory = user2.getInventory();
+        tradeID = tradeID.randomUUID();
+        this.user1 = user1.getUUID();
+        this.user2 = user2.getUUID();
     }
 
     public void tradeBooks(List<Book> user1TradeOffer, List<Book> user2TradeOffer) {
-        if(user2Accepted  && user1Accepted) {
-            addRecievedBooks(user2TradeOffer, user1Inventory);
-            addRecievedBooks(user1TradeOffer, user2Inventory);
+        if(getUser1Accepted()  && getUser2Accepted()) {
+            addReceivedBooks(user2TradeOffer, user1Inventory);
+            addReceivedBooks(user1TradeOffer, user2Inventory);
 
             removeTradedBooks(user1TradeOffer, user1Inventory);
             removeTradedBooks(user2TradeOffer, user2Inventory);
         }
     }
 
-    public void addRecievedBooks(List<Book> bookList, Inventory inventory){
+    public void addReceivedBooks(List<Book> bookList, Inventory inventory){
         for(Book book: bookList){
             inventory.addBook(book);
         }
@@ -36,8 +43,7 @@ public class Trade {
 
     public void removeTradedBooks(List<Book> bookList, Inventory inventory){
         for(Book book: bookList){
-            // Uncomment once books can be removed!
-           // inventory.removeBook(book);
+          inventory.deleteBookByName(book.getName());
         }
     }
 
@@ -54,6 +60,6 @@ public class Trade {
     }
 
     public Boolean getUser2Accepted(){
-        return this.getUser2Accepted();
+        return this.user2Accepted;
     }
 }
