12a30b5f0c21d55eb3350407718ffc1dc842aa1a
Mason <mstrong@ualberta.ca>
2015-11-05 09:19:21 -0700

    extended friendslist to display friends, will need to be modified to properly retrieve friends from friendslist object

--- Mybrary/app/src/main/java/com/example/jacob/mybrary/FriendsListActivity.java
+++ Mybrary/app/src/main/java/com/example/jacob/mybrary/FriendsListActivity.java
@@ -7,14 +7,37 @@ import android.os.Bundle;
 import android.text.InputType;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 public class FriendsListActivity extends AppCompatActivity {
+
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends_list);
+
+        /*This should be replaced with data manager code to grab the users friend list*/
+        /*
+        FriendsList friendsList = new FriendsList();
+        friendsList.addFriend(new User("Dominieque", "123", "M", "234"));
+        friendsList.addFriend(new User("Jackylnn", "123", "M", "234"));
+        friendsList.addFriend(new User("Victor", "123", "M", "234"));
+        friendsList.addFriend(new User("Betty", "123", "M", "234"));
+        friendsList.addFriend(new User("Daphne", "123", "M", "234"));
+        */
+        ////remove the above when proper data manager is implemented
+        String[] friends = new String[] {"Dominieque", "Jackylnn", "Victor", "Betty", "Daphne"};
+
+        listView = (ListView) findViewById(R.id.listView);
+
+        //adapter needs to reference a new xml that is just a textView
+        ListAdapter listAdapter = new ArrayAdapter<String>(this, R.layout.simple_list_item, friends);
+        listView.setAdapter(listAdapter);
         fillFriendsList();
     }
 
@@ -40,9 +63,13 @@ public class FriendsListActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+
     public void fillFriendsList(){
 
-        // grab all those friends
+
+
+        //https://developer.xamarin.com/guides/android/user_interface/working_with_listviews_and_adapters/part_2_-_populating_a_listview_with_data/
+
 
     }
 
