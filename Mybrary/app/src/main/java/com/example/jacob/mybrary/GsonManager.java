7f09626a37eff2a237829e04d7d0061e676061e7
Jacobchar <jj.charlebois@gmail.com>
2015-11-06 11:53:40 -0700

    Created Gson Manager

--- Mybrary/app/src/main/java/com/example/jacob/mybrary/GsonManager.java
+++ Mybrary/app/src/main/java/com/example/jacob/mybrary/GsonManager.java
@@ -2,11 +2,38 @@ package com.example.jacob.mybrary;
 
 
 import com.google.gson.Gson;
+import com.google.gson.JsonElement;
 import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
 
 /**
  * Created by Jacob on 2015-11-04.
  */
 public class GsonManager {
-    // toJson and fromJson are methods already implemented in the Gson library.
+
+    private static GsonManager instance = null;
+
+    protected GsonManager(){}
+
+    public static GsonManager getInstance() {
+        if(instance == null) {
+            instance = new GsonManager();
+        }
+        return instance;
+    }
+
+    public String toJson(Object object){
+
+        Gson gson = new Gson();
+        return gson.toJson(object);
+    }
+
+    public Type fromJson(JsonElement json, Type typeOfT){
+
+        Gson gson = new Gson();
+        return gson.fromJson(json, typeOfT);
+
+    }
+
 }
