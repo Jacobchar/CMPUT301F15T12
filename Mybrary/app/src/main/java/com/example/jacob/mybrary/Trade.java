312f2ac7bbcd1d65301c133e4c191a6a133071c8
David <dav1d@shaw.ca>
2015-11-06 11:13:05 -0700

    Added javadoc comments to Trade

--- Mybrary/app/src/main/java/com/example/jacob/mybrary/Trade.java
+++ Mybrary/app/src/main/java/com/example/jacob/mybrary/Trade.java
@@ -18,6 +18,11 @@ public class Trade {
     Boolean user2Accepted = false;
     UUID tradeID;
 
+    /**
+     * Trade between two users
+     * @param user1 One user who is taking part of the trade
+     * @param user2 A second user who is taking part in the trade
+     */
     public Trade(User user1, User user2){
         user1Inventory = user1.getInventory();
         user2Inventory = user2.getInventory();
@@ -26,6 +31,11 @@ public class Trade {
         this.user2 = user2.getUUID();
     }
 
+    /**
+     * Perform the trade between users, using a list of books that they both want to trade
+     * @param user1TradeOffer List of books that user 1 wishes to trade
+     * @param user2TradeOffer List of books that user 2 wishes to trade
+     */
     public void tradeBooks(List<Book> user1TradeOffer, List<Book> user2TradeOffer) {
         if(getUser1Accepted()  && getUser2Accepted()) {
             addReceivedBooks(user2TradeOffer, user1Inventory);
@@ -36,30 +46,56 @@ public class Trade {
         }
     }
 
-    public void addReceivedBooks(List<Book> bookList, Inventory inventory){
+    /**
+     * Add new books to the users inventory
+     * @param bookList List of books that the user will be receiving
+     * @param inventory Inventory of the user receiving the books
+     */
+    private void addReceivedBooks(List<Book> bookList, Inventory inventory){
         for(Book book: bookList){
             inventory.addBook(book);
         }
     }
 
-    public void removeTradedBooks(List<Book> bookList, Inventory inventory){
+    /**
+     * Remove old books from the users inventory
+     * @param bookList List of books that will be removed from the user
+     * @param inventory Inventory of the user having books removed
+     */
+    private void removeTradedBooks(List<Book> bookList, Inventory inventory){
         for(Book book: bookList){
           inventory.deleteBookByName(book.getName());
         }
     }
 
+    /**
+     * Set whether user 1 has accepted the current trade offer
+     * @param accepted True or False as to whether the trade has been accepted
+     */
     public void setUser1Accepted(Boolean accepted){
         user1Accepted = accepted;
     }
 
+    /**
+     * Set whether user 2 has accepted the current trade offer
+     * @param accepted True of False as to whether the trade has been accepted
+     */
     public void setUser2Accepted(Boolean accepted){
         user2Accepted = accepted;
     }
 
+    /**
+     * Get the accept status for the current trade offer
+     * @return Whether user 1 has accepted the current trade
+     */
     public Boolean getUser1Accepted(){
         return this.user1Accepted;
     }
 
+    /**
+     * Get the accept status for the current trade offer
+     * @return Whether user 2 has accepted the current trade
+     */
     public Boolean getUser2Accepted(){
         return this.user2Accepted;
     }
