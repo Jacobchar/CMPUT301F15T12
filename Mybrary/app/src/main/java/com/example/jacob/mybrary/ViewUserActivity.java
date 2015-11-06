fa38a664897e8d150a3673d3790426c2bdcf899b
Ben Schreiber <bschreib@ualberta.ca>
2015-11-06 11:24:11 -0700

    added doc to User

--- Mybrary/app/src/main/java/com/example/jacob/mybrary/ViewUserActivity.java
+++ Mybrary/app/src/main/java/com/example/jacob/mybrary/ViewUserActivity.java
@@ -1,22 +1,10 @@
 package com.example.jacob.mybrary;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.ScrollView;
 import android.widget.TextView;
 
-import com.google.gson.Gson;
-
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class ViewUserActivity extends AppCompatActivity {
 
@@ -27,7 +15,7 @@ public class ViewUserActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_user);
         //loadUser(myUser);
-        //Test
+        //Test until loading user is implemented
         myUser = new User("Name", "Email", "4121", "Gender", "Bio", "City");
         setText(findViewById(R.id.textLayout));
     }
