e6dd25b3e288c9a3fabff9e7da33e5aab57bc0a3
Mason <mstrong@ualberta.ca>
2015-11-19 18:11:51 -0700

    runnable tasks that modify model off UI thread implemented, need to get adapter to update properly

---
+++ Mybrary/app/src/main/java/com/example/jacob/mybrary/FriendListController.java
@@ -0,0 +1,81 @@
+package com.example.jacob.mybrary;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * Created by Mason Strong on 11/18/2015.
+ */
+public class FriendListController {
+    DataManager dataManager = DataManager.getInstance();
+    ConnectionManager connectionManager = ConnectionManager.getInstance();
+    ArrayList<User> userList = new ArrayList<>();
+    LocalUser localUser = LocalUser.getInstance();
+
+    FriendListController(){
+
+    }
+
+
+    public void removeFriend(String friend, Context context) {
+        class oneOff implements Runnable{
+            String friend;
+            Context context;
+
+            oneOff(String f, Context c){
+                friend = f;
+                context = c;
+            }
+
+            public void run() {
+                connectionManager.checkNetwork(context);
+                try {
+                    //remove friend based on username from localUser
+                    localUser.getFriendsList().removeFriend(dataManager.searchUsers(friend).get(0));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return;
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    return;
+                }
+            }
+        }
+    }
+
+    public void addNewFriend(String friend, Context context) {
+        class oneOff implements Runnable{
+            String friend;
+            Context context;
+
+            oneOff(String f, Context c){
+                friend = f;
+                context = c;
+            }
+
+            public void run() {
+                connectionManager.checkNetwork(context);
+                try {
+                    userList = dataManager.searchUsers(friend);
+                    if (userList.size()!=0){
+                        localUser.getFriendsList().addFriend(userList.get(0));
+                    }
+                    else {
+                        //notify friend not found?
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return;
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    return;
+                }
+            }
+        }
+    }
+}
--- Mybrary/app/src/main/java/com/example/jacob/mybrary/FriendsListActivity.java
+++ Mybrary/app/src/main/java/com/example/jacob/mybrary/FriendsListActivity.java
@@ -2,6 +2,7 @@ package com.example.jacob.mybrary;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -23,7 +24,9 @@ import java.util.ArrayList;
 public class FriendsListActivity extends AppCompatActivity {
 
     private ListView listView;
-    private ListAdapter listAdapter;
+    private ArrayAdapter<String> listAdapter;
+    private AlertDialog infoDialog;
+    private FriendListController friendListController = new FriendListController();
     LocalUser localUser = LocalUser.getInstance();
     DataManager dataManager = DataManager.getInstance();
 
@@ -32,8 +35,39 @@ public class FriendsListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends_list);
 
-        listView = (ListView) findViewById(R.id.listView);
         fillFriendsList();
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> arg0, View arg1, int pos, long id) {
+
+                final String name = (String) listView.getItemAtPosition(pos);
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(FriendsListActivity.this);
+                builder.setMessage("Unfriend " + name + "?");
+                builder.setCancelable(true);
+                builder.setNegativeButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        //Delete friend from friendlist object
+
+                        friendListController.removeFriend(name, FriendsListActivity.this);
+
+                        //adapter.notifyDataSetChanged();
+                        dialog.cancel();
+                    }
+                });
+
+                builder.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+                infoDialog = builder.create();
+                infoDialog.show();
+                return true;
+            }
+        });
     }
 
     @Override
@@ -60,16 +94,15 @@ public class FriendsListActivity extends AppCompatActivity {
 
 
     public void fillFriendsList(){
+        listView = (ListView) findViewById(R.id.listView);
         ArrayList<String> friends = localUser.getFriendsList().getNames();
 
         //= new String[] {"Dominieque", "Jackylnn", "Victor", "Betty", "Daphne"};
 
         listAdapter = new ArrayAdapter<String>(this, R.layout.simple_list_item, friends);
         listView.setAdapter(listAdapter);
-        // listAdapter.notifyDataSetChanged();
-
-        //https://developer.xamarin.com/guides/android/user_interface/working_with_listviews_and_adapters/part_2_-_populating_a_listview_with_data/
-    }
+        //listAdapter.notify();
+     }
 
     public void deleteFriend(){
 
@@ -81,43 +114,30 @@ public class FriendsListActivity extends AppCompatActivity {
 
         String promptString = "Enter the username of the friend you'd like to add: ";
 
-        final AlertDialog infoDialog = new AlertDialog.Builder(this).create();
+        infoDialog = new AlertDialog.Builder(this).create();
         infoDialog.setMessage(promptString);
 
         final EditText input = new EditText(this);
         input.setInputType(InputType.TYPE_CLASS_TEXT);
         infoDialog.setView(input);
-
         infoDialog.setButton(DialogInterface.BUTTON_POSITIVE,
                 "Search", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
                         String username = input.getText().toString();
-                        ArrayList<User> userList = new ArrayList<User>();
 
-                        //This needs to be made to not cause a networkOnMainThread exception....
-                        /*
+
+                        //Add friend on separate thread
+                        friendListController.addNewFriend(username, FriendsListActivity.this);
+
                         try {
-                            userList = dataManager.searchUsers(username);
+                            dataManager.saveLocalUser();
                         } catch (IOException e) {
                             e.printStackTrace();
-                            return;
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                            return;
                         }
-*/
-                        if (userList.size() != 0) {
-                            localUser.getFriendsList().addFriend(userList.get(0));
-                            infoDialog.setMessage(username + " added as friend!");
-                            try {
-                                dataManager.saveLocalUser();
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        } else {
-                            infoDialog.setMessage("User not found.");
-                        }
+
+                        //TODO: update UI appropriately. Using the adapter? Synchronization issue + crashes...
+                        //listAdapter.notify();
                     }
                 });
 
