21db6b12d8271d17e8a0ab49076858d54640a857
oksmarty <hessdorf@ualberta.ca>
2015-11-29 13:31:49 -0700

    Added item cloning functionality on long click. Will test when server is up again.

--- Mybrary/app/src/main/java/com/example/jacob/mybrary/ViewGivenInventoryActivity.java
+++ Mybrary/app/src/main/java/com/example/jacob/mybrary/ViewGivenInventoryActivity.java
@@ -13,6 +13,7 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.sql.Connection;
 import java.util.UUID;
 
 public class ViewGivenInventoryActivity extends AppCompatActivity {
@@ -23,6 +24,7 @@ public class ViewGivenInventoryActivity extends AppCompatActivity {
     private Inventory inventory;
     private Activity activity;
     private String activityTitle = "";
+    private ConnectionManager connectionManager = ConnectionManager.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +51,44 @@ public class ViewGivenInventoryActivity extends AppCompatActivity {
             }
         });
         thread.start();
+
+        inventoryListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> arg0, View arg1, int pos, long id) {
+
+                final Book book = (Book) inventoryListView.getItemAtPosition(pos);
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(ViewGivenInventoryActivity.this);
+                builder.setMessage("Would you like to clone this item into your inventory?");
+                builder.setCancelable(true);
+
+                builder.setPositiveButton("Clone Item", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Thread thread = new Thread(new Runnable() {
+                            @Override
+                            public void run() {
+                                connectionManager.updateConnectivity(activity);
+                                inventoryController.cloneItem(book);
+
+                            }
+                        });
+                        thread.start();
+                        dialog.cancel();
+                    }
+                });
+
+                builder.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+                AlertDialog alert = builder.create();
+                alert.show();
+
+                return true;
+            }
+        });
 
     }
 
